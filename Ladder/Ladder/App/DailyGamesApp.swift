25bb1f56e6a9b588a7038bd552b68186cf5ffea3
MasonLiebe <149519733+MasonLiebe@users.noreply.github.com>
2025-12-23 21:56:01 -0600

    create xcode project

---
+++ Ladder/Ladder/App/DailyGamesApp.swift
@@ -0,0 +1,20 @@
+//
+//  DailyGamesApp.swift
+//  Ladder
+//
+//  Created by Mason Liebe on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                PlayMenuView()
+            }
+        }
+    }
+}
+
---
+++ Ladder/LadderTests/LadderTests.swift
@@ -0,0 +1,17 @@
+//
+//  LadderTests.swift
+//  LadderTests
+//
+//  Created by Mason Liebe on 12/23/25.
+//
+
+import Testing
+@testable import Ladder
+
+struct LadderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
